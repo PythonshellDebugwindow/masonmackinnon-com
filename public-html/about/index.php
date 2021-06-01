1f350ffd56b79d60a63ba77fe769ea7809598572
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-05-31 20:44:31 -0400

    Create index.php

---
+++ public-html/about/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About - MasonMackinnon.Com</title>
+        <link rel="stylesheet" href="/styles/main.css" />
+    </head>
+    <body>
+        <?php include_once("../../includes/header.php") ?>
+        
+        <section>
+            <div>
+                <h1 class="center-text darkred">About</h1>
+                <p class="justify">
+                    Hello, dear visitor. I am Mason MacKinnon. This is my website. It <abbr title="will have been">has been</abbr> coded by hand and computer in HTML, JS, CSS, PHP, SQL, Apache, Math, and English.
+                    This means that any bugs&amp;#200b;, spelign mistaces, and other <s>errors</s> features are almost certainly entirely my fault.
+                </p>
+            </div>
+        </section>
+        
+        <?php include_once("../../includes/footer.php") ?>
+    </body>
+</html>
