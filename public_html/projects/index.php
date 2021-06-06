d73353ab08eb8cf143df0b185558788a06e127a0
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-05 20:04:56 -0400

    Create index.php
    Yes, I am using an iPad

---
+++ public_html/projects/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projects | MasonMackinnon.Com</title>
+        <link rel="stylesheet" href="/styles/main.css" />
+        <link rel="stylesheet" href="/styles/container-grid.css" />
+    </head>
+    <body>
+        <?php include_once("../../includes/header.php") ?>
+        
+        <section>
+            <h1>Projects</h1>
+            
+            <div class="container-grid">
+                <div class="item">
+                    <h2><a href="https://replit.com/@MasonALM/socketio-Snakeish-Game" rel="noopener noreferrer">Multiplayer Snake</a></h2>
+                    <p>This game was made with JQuery, NodeJS and Socket.io. Multiple people can play it.</p>
+                </div>
+                <div class="item">
+                    <h2>Space Invaders</h2>
+                    <p>I made this game with Python and Pygame. Don't shoot your shields!</p>
+                </div>
+                <div class="item">
+                    <h2>Platformer</h2>
+                    <p>Also made with Python and Pygame. Finish the level by jumping out of the world.</p>
+                </div>
+                <div class="item">
+                    <h2><a href="https://github.com/PythonshellDebugwindow/py-games/tree/master/PacMan" rel="noopener noreferrer">PacMan</a></h2>
+                    <p>A typical Pacman game where the ghosts mainly ignore you.</p>
+                </div>
+                <div class="item">
+                    <h2>Pong3D</h2>
+                    <p>Made with Python, OpenGL, and Pygame, this is a 3D game of Pong. The computer cannot be beaten.</p>
+                </div>
+                <div class="item">
+                    <h2><a href="webpong.php">WebPong</a></h2>
+                    <p>Made with JavaScript, this is a 2D game of Pong. You can play it in your browser (if you have JavaScript enabled).</p>
+                </div>
+                <div class="item">
+                    <h2><a href="https://replit.com/@MasonALM/Text-adventure" rel="noopener noreferrer">Text Adventure</a></h2>
+                    <p>A text adventure made using Python and Replit. It is currently very short.</p>
+                </div>
+                <div class="item">
+                    <h2>GUIP</h2>
+                    <p>A LISP-like language implemented in Python and Pygame. Stands for GUI Processing, although it really isn't very good for making GUIS.</p>
+                </div>
+                <div class="item">
+                    <h2><a href="https://www.github.com/PythonshellDebugwindow/esolangs" rel="noopener noreferrer">Esolangs</a></h2>
+                    <p>This is a collection of my implementations of esolangs (esoteric programming languages), excluding GUIP and a few others.</p>
+                </div>
+                <div class="item">
+                    <h2><a href="/">MasonMackinnon.Com</a></h2>
+                    <p>You might not know it, but you're looking at it right now!</p>
+                </div>
+            </div>
+        </section>
+        
+        <?php include_once("../../includes/footer.php") ?>
+    </body>
+</html>
