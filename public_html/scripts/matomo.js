44e44d939d97f22c1f78359278bf3075b225d285
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-05 20:08:06 -0400

    Create matomo.js

---
+++ public_html/scripts/matomo.js
@@ -0,0 +1,15 @@
+if(localStorage && localStorage.getItem("useCookies") !== "No")
+{
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+    var u="//masonmackinnon.com/matomo/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+}
+else console.log("Cookies are disabled or unavailable.");
