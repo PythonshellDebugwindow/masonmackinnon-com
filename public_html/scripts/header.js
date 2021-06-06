58f39d64d3ef82b7724565efdf09f158b9d30bfc
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-05 20:07:23 -0400

    Create header.js
    And with it, the scripts directory

---
+++ public_html/scripts/header.js
@@ -0,0 +1,13 @@
+const mainToggle = document.querySelector(".header-top .main-toggle");
+const inlineToggle = document.querySelector(".header-top .inline-toggle");
+const ul = document.querySelector(".header-top ul");
+
+function toggle()
+{
+    document.body.classList.toggle("toggle-active");
+    mainToggle.classList.toggle("active");
+    ul.classList.toggle("active");
+    return false; //Stop propagation
+}
+mainToggle.onclick = toggle;
+inlineToggle.onclick = toggle;
