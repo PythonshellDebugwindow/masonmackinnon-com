07aec0c8abdd40c717607ea25d30d0edd3b57dff
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-26 20:04:41 -0400

    Update index.php

--- public_html/contact/index.php
+++ public_html/contact/index.php
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Contact - MasonMackinnon.Com</title>
-        <link rel="stylesheet" href="/styles/main.css" />
-    </head>
-    <body>
-        <?php include_once("../../includes/header.php") ?>
-        
-        <section class="center-items full-page">
-            <div>
-                <h1 class="center-text darkred bigger">Contact</h1>
-                <p class="big">You can contact me by email at <a href="mailto:pythonshelldebugwindow@gmail.com" rel="noopener noreferrer" class="red">pythonshelldebugwindow at gmail dot com</a>.</p>
-                <h2 class="center-text darkred bigger">External Resources</h2>
-                <p class="big center-text">
-                    <a href="https://www.github.com/PythonshellDebugwindow" rel="noopener noreferrer">GitHub</a>
-                </p>
-            </div>
-        </section>
-        
-        <?php include_once("../../includes/footer.php") ?>
-    </body>
-</html>
+<?php
+    $title = 'Contact';
+    include_once("../../includes/header.php")
+?>
+<section class="center-items full-page">
+    <div>
+        <h1 class="center-text darkred bigger">Contact</h1>
+        <p class="big">You can contact me by email at <a href="mailto:pythonshelldebugwindow@gmail.com" rel="noopener noreferrer" class="red">pythonshelldebugwindow at gmail dot com</a>.</p>
+        <h2 class="center-text darkred bigger">External Resources</h2>
+        <p class="big center-text">
+            <a href="https://www.github.com/PythonshellDebugwindow" rel="noopener noreferrer">GitHub</a>
+        </p>
+    </div>
+</section>
+<?php include_once("../../includes/footer.php") ?>
