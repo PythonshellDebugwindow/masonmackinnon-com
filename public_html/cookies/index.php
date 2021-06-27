31afcf2f614b06fbf21b12306e59f6382e097fa6
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-26 20:06:22 -0400

    Update index.php

--- public_html/cookies/index.php
+++ public_html/cookies/index.php
@@ -1,51 +1,42 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cookies - MasonMackinnon.Com</title>
-        <link rel="stylesheet" href="/styles/main.css" />
-    </head>
-    <body>
-        <?php include_once("../../includes/header.php") ?>
-        
-        <section>
-            <div>
-                <h1 class="center-text darkred">Cookies</h1>
-                <p class="justify">
-                    This site uses cookies for analytical purposes (records number of visitors, actions per visit, etc). Click <button id="tc">here</button> to toggle them. They are currently <span id="hc"></span><noscript>disabled because JavaScript is disabled.</noscript>. (Note: Disabling cookies will leave one cookie in localStorage, <tt>hasCookies</tt>, to remember your preference. Browse without JavaScript to disable it.)
-                </p>
-            </div>
-        </section>
-        
-        <script>
-            document.getElementById("tc").onclick = function()
-            {
-                //useCookies is the name
-                if(!localStorage)
-                {
-                    document.cookie = "";
-                    document.getElementById("hc").innerHTML = "disabled (needs localStorage to enable)";
-                    return;
-                }
-                var hasCookies = localStorage.getItem("useCookies") !== "No";
-                var newHasCookies = hasCookies ? "No" : "Yes";
-                if(!hasCookies)
-                {
-                    document.cookie = "";
-                    localStorage.clear();
-                }
-                var hasCookiesMsg = newHasCookies === "Yes" ? "enabled" : "disabled";
-                localStorage.setItem("useCookies", newHasCookies);
-                document.getElementById("hc").innerHTML = hasCookiesMsg;
-            };
-            if(localStorage)
-            {
-                var hasCookies = localStorage.getItem("useCookies") !== "No";
-                var hasCookiesMsg = hasCookies ? "enabled" : "disabled";
-                document.getElementById("hc").innerHTML = hasCookiesMsg;
-            }
-        </script>
-        <?php include_once("../../includes/footer.php") ?>
-    </body>
-</html>
+<?php
+    $title = 'Cookies';
+    include_once("../../includes/header.php")
+?>
+<section>
+    <div>
+        <h1 class="center-text darkred">Cookies</h1>
+        <p class="justify">
+            This site uses cookies for analytical purposes (records number of visitors, actions per visit, etc). Click <button id="tc">here</button> to toggle them. They are currently <span id="hc"></span><noscript>disabled because JavaScript is disabled.</noscript>. (Note: Disabling cookies will leave one cookie in localStorage, <tt>hasCookies</tt>, to remember your preference. Browse without JavaScript to disable it.)
+        </p>
+    </div>
+</section>
+
+<script>
+    document.getElementById("tc").onclick = function()
+    {
+        //useCookies is the name
+        if(!localStorage)
+        {
+            document.cookie = "";
+            document.getElementById("hc").innerHTML = "disabled (needs localStorage to enable)";
+            return;
+        }
+        var hasCookies = localStorage.getItem("useCookies") !== "No";
+        var newHasCookies = hasCookies ? "No" : "Yes";
+        if(!hasCookies)
+        {
+            document.cookie = "";
+            localStorage.clear();
+        }
+        var hasCookiesMsg = newHasCookies === "Yes" ? "enabled" : "disabled";
+        localStorage.setItem("useCookies", newHasCookies);
+        document.getElementById("hc").innerHTML = hasCookiesMsg;
+    };
+    if(localStorage)
+    {
+        var hasCookies = localStorage.getItem("useCookies") !== "No";
+        var hasCookiesMsg = hasCookies ? "enabled" : "disabled";
+        document.getElementById("hc").innerHTML = hasCookiesMsg;
+    }
+</script>
+<?php include_once("../../includes/footer.php") ?>
