62433df0c0a159ec0ef8a72a7138d21952a3de2b
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-26 20:09:21 -0400

    Update index.php

--- public_html/logout/index.php
+++ public_html/logout/index.php
@@ -1,27 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>User - MasonMackinnon.Com</title>
-        <link rel="stylesheet" href="/styles/main.css" />
-    </head>
-    <body>
-        <?php
-            include_once("../../includes/header.php");
-            session_start();
-            unset($_SESSION['id']);
-            unset($_SESSION['name']);
-            session_destroy();
-        ?>
-        
-        <section class="center-items">
-            <div>
-                <h1 class="center-text">Logged out successfully.</h1>
-                <p class="center-text">Some pages might still appear as if you were logged in until your clear your browser's cache.</p>
-            </div>
-        </section>
-        
-        <?php include_once("../../includes/footer.php") ?>
-    </body>
-</html>
+<?php
+    $title = 'Log Out';
+    include_once("../../includes/header.php");
+    session_start();
+    unset($_SESSION['id']);
+    unset($_SESSION['name']);
+    session_destroy();
+?>
+<section class="center-items">
+    <div>
+        <h1 class="center-text">Logged out successfully.</h1>
+        <p class="center-text">Some pages might still appear as if you were logged in until your clear your browser's cache.</p>
+    </div>
+</section>
+<?php include_once("../../includes/footer.php") ?>
