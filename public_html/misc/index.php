4f7624401304c50e7292ba0525b508c49854a705
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-06-26 20:10:50 -0400

    Update index.php

--- public_html/misc/index.php
+++ public_html/misc/index.php
@@ -1,33 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Miscellany | MasonMackinnon.Com</title>
-        <link rel="stylesheet" href="/styles/main.css" />
-        <link rel="stylesheet" href="/styles/container-grid.css" />
-    </head>
-    <body>
-        <?php include_once("../../includes/header.php") ?>
-        
-        <section>
-            <h1>Miscellany</h1>
-            <div class="container-grid">
-                <div class="item">
-                    <h2><a href="terrain.php">Terrain Generation</a></h2>
-                    <p>A terrain generator. It generates terrain: mountains, oceans, and plains.</p>
-                </div>
-                <div class="item">
-                    <h2><a href="words.php">Words</a></h2>
-                    <p>A list of keywords from across programming languages.</p>
-                </div>
-                <div class="item">
-                    <h2>Item 3</h2>
-                    <p>Content</p>
-                </div>
-            </div>
-        </section>
-        
-        <?php include_once("../../includes/footer.php") ?>
-    </body>
-</html>
+<?php
+    $title = 'Miscellany';
+    $styles = array('../styles/container-grid');
+    include_once("../../includes/header.php")
+?>
+<section>
+    <h1>Miscellany</h1>
+    <div class="container-grid">
+        <div class="item">
+            <h2><a href="terrain.php">Terrain Generator</a></h2>
+            <p>A terrain generator. It generates terrain: mountains, oceans, and plains.</p>
+        </div>
+        <div class="item">
+            <h2><a href="words.php">Words</a></h2>
+            <p>A list of keywords from across programming languages.</p>
+        </div>
+        <div class="item">
+            <h2>Item 3</h2>
+            <p>Content</p>
+        </div>
+    </div>
+</section>
+<?php include_once("../../includes/footer.php") ?>
