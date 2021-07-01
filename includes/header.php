174fbdb9b9842f0140612138dbfddac24340b1db
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-07-01 16:43:48 -0400

    Add blog

--- includes/header.php
+++ includes/header.php
@@ -27,6 +27,7 @@
     <div class="toggle main-toggle"></div>
     <ul>
         <li><a href="/about/">About</a></li>
+        <li><a href="/blog/">Blog</a></li>
         <li><a href="/projects/">Projects</a></li>
         <li><a href="/trivia/">Trivia</a></li>
         <li><a href="/misc/">Miscellany</a></li>
