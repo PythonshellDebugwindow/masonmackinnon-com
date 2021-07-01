2a3b218a4a61b671e03a16fddf3c2cff42f34e43
Pythonshell Debugwindow <65022661+PythonshellDebugwindow@users.noreply.github.com>
2021-07-01 11:33:37 -0400

    TLAO

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,7 @@
             <ul>
                 <li><a href="/about/">About</a></li>
                 <li><a href="/blog/">Blog</a></li>
+                <li><a href="/blog/?t">Two Languages At Once</a></li>
                 <li><a href="/projects/">Projects</a></li>
                 <li><a href="/trivia/">Trivia</a></li>
                 <li><a href="/misc/">Miscellany</a></li>
